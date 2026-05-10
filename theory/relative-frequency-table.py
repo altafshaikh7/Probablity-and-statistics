a8cf500e22bb95e0a264ddf9b58de5fbb25f65dd
altafshaikh7 <altafshaikh07781@gmail.com>
2026-05-10 07:41:39 +0530

    relative frequency table

---
+++ theory/relative-frequency-table.py
@@ -0,0 +1,24 @@
+# A Relative Frequency Table in Statistics shows the proportion or percentage of each category instead of just the frequency (count).
+# It tells how much each category contributes to the total data.
+
+
+# | Subject | Frequency | Relative Frequency |
+# | ------- | --------- | ------------------ |
+# | Math    | 10        | 10/25 = 0.40       |
+# | Science | 8         | 8/25 = 0.32        |
+# | English | 7         | 7/25 = 0.28        |
+
+
+# Understanding
+# 0.40 means 40% students like Math
+# 0.32 means 32% students like Science
+
+
+
+# Uses
+
+# Comparing proportions
+# Finding probabilities
+# Data analysis and surveys
+# Creating pie charts and statistics reports
+
