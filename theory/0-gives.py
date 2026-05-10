47fdd41917f891b7b1a146ab3a85b396257a93b4
altafshaikh7 <altafshaikh07781@gmail.com>
2026-05-10 07:34:02 +0530

    O-Gives

---
+++ theory/0-gives.py
@@ -0,0 +1,32 @@
+# An Ogive is a graph used in Statistics to represent the cumulative frequency distribution of data.
+
+# It is also called a Cumulative Frequency Curve.
+
+# Types of Ogives
+# Less Than Ogive
+# Shows cumulative frequencies less than upper class limits.
+# More Than Ogive
+# Shows cumulative frequencies greater than lower class limits.
+
+# Main Features
+# Data points are connected by a smooth curve
+# Helps find:
+# Median
+# Quartiles
+# Percentiles
+# Based on cumulative frequency
+# Example
+
+# If cumulative frequencies are:
+
+# Marks	Cumulative Frequency
+# 10	5
+# 20	12
+# 30	20
+
+# These points are plotted and joined to form an ogive.
+
+# Uses
+# Finding median graphically
+# Understanding cumulative data
+# Comparing distributions
