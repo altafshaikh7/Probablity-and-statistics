61c2eaae1b3b62cf1ce4f6b2e33ecd2b3df2fe0a
altafshaikh7 <altafshaikh07781@gmail.com>
2026-05-10 07:29:02 +0530

    bar graph

---
+++ theory/bar-graph.py
@@ -0,0 +1,27 @@
+# A Bar Graph is a graphical representation used in Statistics to compare different categories using rectangular bars.
+
+# The length or height of each bar represents the value or frequency.
+
+# Example
+# Subject	Students
+# Math	    12
+# Science	8
+# English	5
+
+# In a bar graph:
+
+# Subjects are shown on the X-axis
+# Number of students on the Y-axis
+# Taller bar = greater value
+
+# Features
+# Bars have equal width
+# Bars are separated by spaces
+# Can be vertical or horizontal
+# Easy for comparison
+
+# Uses
+# Comparing data
+# Showing frequencies
+# Visualizing survey results
+# Business and statistical analysis
