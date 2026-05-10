ae0bb2c8804739214cfc745a562019aa627dd448
altafshaikh7 <altafshaikh07781@gmail.com>
2026-05-10 07:44:36 +0530

    joint distribution

---
+++ theory/joint-distribution.py
@@ -0,0 +1,32 @@
+# A Joint Distribution in Probability and Statistics shows the probability distribution of two random variables together.
+
+# It tells the probability of two events happening at the same time.
+
+# Example
+# Suppose:
+
+# X = result of first coin
+# Y = result of second coin
+
+# Possible outcomes:
+# | X | Y | Probability |
+# | - | - | ----------- |
+# | H | H | 1/4         |
+# | H | T | 1/4         |
+# | T | H | 1/4         |
+# | T | T | 1/4         |
+
+# This table represents the joint distribution of XXX and YYY.
+
+# Joint Probability Formula
+# P(X=x, Y=y)P(X=x,\ Y=y)P(X=x, Y=y)
+# It means:
+# Probability that X=xX=xX=x and Y=yY=yY=y occur together.
+
+# Uses
+# Studying relationships between variables
+# Probability calculations
+# Machine learning and statistics
+# Data analysis
+
+
