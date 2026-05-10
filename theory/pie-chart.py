901e79bf7c7e07708158dff0921fcaa99a00cddb
altafshaikh7 <altafshaikh07781@gmail.com>
2026-05-10 07:31:01 +0530

    pie chart

---
+++ theory/pie-chart.py
@@ -0,0 +1,28 @@
+# A Pie Chart is a circular graph used in Statistics to show how a whole is divided into different parts.
+
+# The circle is divided into slices/sectors, and each slice represents a category’s proportion or percentage.
+
+# Example
+
+# If a class has:
+
+# Math = 40%
+# Science = 30%
+# English = 20%
+# Computer = 10%
+
+# then each subject is shown as a slice of the circle.
+
+# Main Features
+# Represents data as parts of a whole
+# Total circle = 100%
+# Larger slice = larger proportion
+# Easy to understand percentages visually
+
+
+
+# Uses
+# Showing percentages
+# Comparing proportions
+# Business reports and surveys
+# Budget and population analysis
