31ebe54180e7fca1f42bb0c710d9c423c2189d45
altafshaikh7 <altafshaikh07781@gmail.com>
2026-05-10 07:25:07 +0530

    one way table

---
+++ one-way-table.py
@@ -0,0 +1,46 @@
+# A One-Way Table in Probability and Statistics is a table that shows the frequency (count) of only one variable.
+
+# It is also called a frequency table.
+
+# Example
+
+# Suppose we collect data about students’ favorite subjects:
+
+# Subject	Number of Students
+# Math	        12
+# Science	    8
+# English	    5
+# Computer	    10
+
+# This table contains information about one characteristic only → favorite subject.
+
+# So, it is called a One-Way Table.
+
+# Main Points
+# Represents one variable
+# Shows frequency/count
+# Easy to understand data distribution
+# Used in probability and statistics for basic analysis
+# Formula for Probability from One-Way Table
+
+# Probability of an event:
+
+# P(E)=
+# Total Frequency
+# Frequency of Event
+# 	​
+
+
+# Example:
+# If total students = 35 and Math lovers = 12,
+
+# P(Math)=
+# 35
+# 12
+# 	​
+
+# Uses
+# Data organization
+# Finding probabilities
+# Comparing frequencies
+# Understanding distributions quickly
