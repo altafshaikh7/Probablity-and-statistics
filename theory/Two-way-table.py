c67122a007a8f106fe592b34c22cc2527bbc7376
altafshaikh7 <altafshaikh07781@gmail.com>
2026-05-10 07:37:54 +0530

    Two way table

---
+++ theory/Two-way-table.py
@@ -0,0 +1,52 @@
+# A Two-Way Table (also called a Contingency Table) is a table used in Probability and Statistics to show the relationship between two different variables.
+
+# It organizes data using:
+
+# Rows → first variable
+# Columns → second variable
+# Example
+
+# Suppose a class survey asks students about their favorite sport.
+
+# Gender	Cricket	Football	Total
+# Boys	15	10	25
+# Girls	8	12	20
+# Total	23	22	45
+# Understanding the Table
+
+# Here:
+
+# Variable 1 = Gender
+# Variable 2 = Favorite Sport
+# Meaning of numbers
+# 15 boys like cricket
+# 10 boys like football
+# 8 girls like cricket
+# 12 girls like football
+# Totals
+# Total boys = 25
+# Total girls = 20
+# Total students = 45
+
+# These are called:
+
+# Row totals
+# Column totals
+# Probability from Two-Way Table
+
+
+
+
+# because 23 students like cricket out of 45 total students.
+
+# Why Two-Way Tables are Useful
+# Easy data organization
+# Compare two variables together
+# Find probabilities quickly
+# Used in surveys and statistics
+
+# Simple Difference
+
+# One-Way Table	                     Two-Way Table
+# Shows one variable	        Shows two variables
+# Simple frequency data	    Relationship between two categories
