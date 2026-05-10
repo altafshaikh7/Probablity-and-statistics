b7a82b79d6b511d030294451967d1536e7052d7b
altafshaikh7 <altafshaikh07781@gmail.com>
2026-05-10 07:32:23 +0530

    line graph

---
+++ theory/line-graph.py
@@ -0,0 +1,23 @@
+# A Line Graph is a graph used in Statistics to show changes in data over time.
+
+# It is formed by plotting points on a graph and joining them with straight lines.
+
+# Example
+# Day	Temperature
+# Monday	30°C
+# Tuesday	32°C
+# Wednesday	35°C
+
+# The points are connected to show how temperature changes day by day.
+
+# Main Features
+# Shows trends and changes clearly
+# Data points are connected by lines
+# X-axis usually represents time
+# Y-axis represents values
+
+# Uses
+# Weather reports
+# Stock market trends
+# Sales growth analysis
+# Scientific and statistical data
