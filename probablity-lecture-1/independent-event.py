9e840d8f6638eac7f61e14cd545f31de74205ac7
altafshaikh7 <altafshaikh07781@gmail.com>
2026-05-15 09:08:09 +0530

    independent events

---
+++ probablity-lecture-1/independent-event.py
@@ -0,0 +1,23 @@
+# Independent Events
+
+# Independent Events in Probability and Statistics are events where the occurrence of one event does not affect the occurrence of another event.
+
+# In simple words:
+
+# One event happening does not change the probability of the other event.
+
+
+# Example
+# Coin Toss and Dice Roll
+# Event A: Getting Head in a coin toss
+# Event B: Getting 4 on a dice
+
+# These events are independent because:
+
+# Tossing the coin does not affect the dice result.
+
+
+# Key Points
+# Events do not influence each other
+# Multiplication rule is used
+# Common in coin, dice, and card probability problems
