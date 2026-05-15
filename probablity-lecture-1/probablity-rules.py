6d88752e733e53c36971b63d595498e49185fe1d
altafshaikh7 <altafshaikh07781@gmail.com>
2026-05-15 09:19:42 +0530

    probablity rules

---
+++ probablity-lecture-1/probablity-rules.py
@@ -0,0 +1,33 @@
+# Probability Rules
+
+# Probability rules in Probability and Statistics are formulas used to calculate the chances of events occurring.
+
+
+
+# 1. Basic Probability Rule
+
+# 0≤P(E)≤1
+
+# Probability is always between 0 and 1.
+# 0 → impossible event
+# 1 → certain event
+
+
+# 2. Probability Formula
+
+# P(E)= Favorable Outcomes/Total Outcomes
+# Used to find probability of an event.
+
+
+# 3. Complement Rule
+
+# P(A′)=1−P(A)
+
+# Where:
+
+# A′= event “not A”
+
+# Example:
+# If probability of rain is 0.7, then probability of no rain:
+
+# 1−0.7=0.3
