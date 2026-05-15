7778cb87c8650e2b3a1334ca4765e580b8e11e57
altafshaikh7 <altafshaikh07781@gmail.com>
2026-05-15 09:01:02 +0530

    basic of probablity

---
+++ probablity-lecture-1/basic.py
@@ -0,0 +1,101 @@
+# Probability is a branch of Probability and Statistics that deals with measuring the chance of occurrence of an event.
+
+# It helps predict how likely an event is to happen.
+
+# The probability value always lies between
+
+# 0≤P(E)≤1
+# 0 → impossible event
+# 1 → certain event
+
+# Basic Formula
+
+# P(E)=
+# Number of Favorable Outcomes
+# --------------------------
+# Total Outcomes
+
+
+# Where:
+
+# P(E) = probability of event E
+
+
+# Important Terms
+# 1. Experiment
+
+# An action that produces outcomes.
+
+# Example:
+
+# Tossing a coin
+# Rolling a dice
+
+
+
+# 2. Outcome
+
+# A possible result of an experiment.
+
+# Example:
+# When rolling a dice:
+
+# 1,2,3,4,5,6
+
+# Each number is an outcome.
+
+
+# 3. Sample Space
+
+# The set of all possible outcomes.
+
+# Example:
+
+# S={H,T}
+
+# for a coin toss.
+    
+
+
+# 4. Event
+
+# A collection of outcomes.
+
+# Example:
+# Getting an even number from dice:
+
+# E={2,4,6}
+
+
+# Types of Probability
+# 1. Theoretical Probability
+
+# Based on mathematical calculation.
+
+# Example:
+# Probability of getting head in coin toss:
+
+# P(H)=1/2
+
+
+# 2. Experimental Probability
+
+# Based on actual experiments and observations.
+
+# Formula:
+
+# P(E)= Number of times event occurs / Total trials
+
+
+# | Event Type       | Meaning                    |
+# | ---------------- | -------------------------- |
+# | Simple Event     | Contains one outcome       |
+# | Compound Event   | Contains multiple outcomes |
+# | Certain Event    | Always occurs              |
+# | Impossible Event | Never occurs               |
+
+
+
+# Addition Rule of Probability 
+
+# Multiplication Rule
