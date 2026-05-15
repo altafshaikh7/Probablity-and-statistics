5a89c5c37c2cda11f7020559d3600a5dfcd1ab5c
altafshaikh7 <altafshaikh07781@gmail.com>
2026-05-15 09:15:50 +0530

    mutually exclusive events

---
+++ probablity-lecture-1/mutually-exclusive-events.py
@@ -0,0 +1,30 @@
+# Mutually Exclusive Events
+
+# Mutually Exclusive Events in Probability and Statistics are events that cannot occur at the same time.
+
+# If one event happens, the other event cannot happen.
+
+
+# Example
+
+# When rolling a dice:
+
+# Event A = getting 2
+# Event B = getting 5
+
+# A dice cannot show both 2 and 5 together in one roll.
+
+# So, these events are mutually exclusive.
+
+
+# Key Points
+# Cannot happen together
+# Intersection probability is zero
+# Addition rule is used
+
+
+
+# Real-Life Examples
+# A person being both alive and dead at the same time
+# Tossing a coin and getting both head and tail together
+# Choosing a card that is both King and Queen simultaneously
