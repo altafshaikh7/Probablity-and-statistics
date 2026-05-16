d02dd4201de11c4692093a87517c083f45787a2f
altafshaikh7 <altafshaikh07781@gmail.com>
2026-05-16 14:19:32 +0530

    bayes theoram

---
+++ probablity-lecture-1/bayes-theoram.py
@@ -0,0 +1,46 @@
+# Bayes’ Theorem
+
+# Bayes' Theorem is an important theorem in Probability Theory used to find the probability of an event based on prior knowledge or conditions.
+
+# It tells us:
+
+# “How to update probability when new information is available.”
+
+
+# Simple Understanding
+
+# Bayes’ theorem helps answer questions like:
+
+# “If a medical test is positive, what is the probability the person actually has the disease?”
+# “If it is cloudy, what is the probability it will rain?”
+# “If a student studied hard, what is the probability they pass?”
+
+# It updates the probability after getting new evidence.
+
+
+# Bayes’ Theorem Tree Idea
+
+# Imagine:
+
+# Start with total population
+# Divide into diseased and healthy
+# Then divide each into positive and negative tests
+
+# Bayes theorem works backward from the evidence.
+
+
+# Applications of Bayes’ Theorem
+# 🏥 Medical Field
+# Disease diagnosis
+
+# 📧 Spam Filters
+# Detect spam emails
+
+# 🤖 Artificial Intelligence
+# Machine learning and prediction
+
+# 🌦 Weather Forecast
+# Rain prediction
+
+# 🔐 Cyber Security
+# Fraud detection
