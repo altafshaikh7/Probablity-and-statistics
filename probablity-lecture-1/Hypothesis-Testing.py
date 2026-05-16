eb09d7643e4724ef1eba5ad533b8ea3264f76e4b
altafshaikh7 <altafshaikh07781@gmail.com>
2026-05-16 14:35:54 +0530

    hypothesis testing

---
+++ probablity-lecture-1/Hypothesis-Testing.py
@@ -0,0 +1,29 @@
+# Statistics hypothesis testing is a method used to make decisions or conclusions about a population using sample data.
+
+# It helps answer questions like:
+
+# Is a new medicine effective?
+# Is a coin fair?
+# Does a new teaching method improve marks?
+
+# Definition
+
+# Hypothesis testing is a statistical method used to test whether a claim about a population is true or not.
+
+# Important Terms
+# | Term                                    | Meaning                                        |
+# | --------------------------------------- | ---------------------------------------------- |
+# | Hypothesis                              | Assumption or claim                            |
+# | Null Hypothesis ((H_0))                 | Default assumption                             |
+# | Alternative Hypothesis ((H_1) or (H_a)) | Opposite claim                                 |
+# | Sample                                  | Small part of population                       |
+# | Significance Level ((\alpha))           | Probability of rejecting true (H_0)            |
+# | Test Statistic                          | Value calculated from sample data              |
+# | p-value                                 | Probability of getting result if (H_0) is true |
+
+
+# Types of Hypothesis
+# | Type                           | Meaning                         |
+# | ------------------------------ | ------------------------------- |
+# | Null Hypothesis ((H_0))        | No effect / no difference       |
+# | Alternative Hypothesis ((H_1)) | There is an effect / difference |
