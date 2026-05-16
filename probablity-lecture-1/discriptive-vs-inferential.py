889a1471c2e66cefeeb71318920c812830d2678c
altafshaikh7 <altafshaikh07781@gmail.com>
2026-05-16 14:30:26 +0530

    Descriptive vs Inferential Statistics

---
+++ probablity-lecture-1/discriptive-vs-inferential.py
@@ -0,0 +1,12 @@
+# | Basis            | Descriptive Statistics         | Inferential Statistics                     |
+# | ---------------- | ------------------------------ | ------------------------------------------ |
+# | Definition       | Describes and summarizes data  | Makes predictions or conclusions from data |
+# | Purpose          | To present data clearly        | To infer about population                  |
+# | Focus            | Existing data                  | Future prediction / decision               |
+# | Data Used        | Complete collected data        | Sample data                                |
+# | Uses Probability | No                             | Yes                                        |
+# | Complexity       | Simple                         | More complex                               |
+# | Result           | Tables, graphs, averages       | Predictions and hypothesis testing         |
+# | Common Methods   | Mean, Median, Mode, Graphs     | Confidence Interval, Hypothesis Testing    |
+# | Example          | Finding average marks of class | Predicting performance of all students     |
+# | Output           | Description of data            | Conclusion about population                |
