962bb72181a51284ad8ad64c8e1c10f69bc47fcf
altafshaikh7 <altafshaikh07781@gmail.com>
2026-05-16 14:36:47 +0530

    hypothesis testing

--- probablity-lecture-1/Hypothesis-Testing.py
+++ probablity-lecture-1/Hypothesis-Testing.py
@@ -27,3 +27,14 @@
 # | ------------------------------ | ------------------------------- |
 # | Null Hypothesis ((H_0))        | No effect / no difference       |
 # | Alternative Hypothesis ((H_1)) | There is an effect / difference |
+
+
+# Steps in Hypothesis Testing
+# | Step | Description                           |
+# | ---- | ------------------------------------- |
+# | 1    | State null and alternative hypotheses |
+# | 2    | Choose significance level ((\alpha))  |
+# | 3    | Collect sample data                   |
+# | 4    | Calculate test statistic              |
+# | 5    | Find p-value / critical value         |
+# | 6    | Accept or reject (H_0)                |
