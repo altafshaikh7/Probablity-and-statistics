d15cadd30c1dd9eec288574db31cdeb6f06a733e
altafshaikh7 <altafshaikh07781@gmail.com>
2026-05-16 14:10:56 +0530

    conditional probablity

---
+++ probablity-lecture-1/conditional-probablity.py
@@ -0,0 +1,32 @@
+# Conditional probability is a concept in Probability Theory that measures the probability of an event occurring given that another event has already happened
+
+# Intuition
+
+# Instead of looking at the entire sample space, conditional probability restricts our focus to cases where B is true, and then asks: within those cases, how often does A happen?
+
+
+# 🎲 Example
+
+# Suppose:
+
+# You pick a card from a deck
+# Event A = “card is a King”
+# Event B = “card is a face card (Jack, Queen, King)”
+
+# We know:
+
+# Total face cards = 12
+# Kings among them = 4
+
+# So:
+
+# P(King∣Face card)  = 4/12 = 0.33
+
+
+# Key Points
+# Conditional probability changes the “universe” you’re working in
+
+# If A and B are independent:
+
+# P(A∣B)=P(A)
+# It is the foundation for important results like Bayes' Theorem
