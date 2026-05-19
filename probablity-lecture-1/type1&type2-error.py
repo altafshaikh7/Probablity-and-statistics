04a002f365aedb909cea8942bee08527520746bb
altafshaikh7 <altafshaikh07781@gmail.com>
2026-05-19 13:04:07 +0530

    type 1 and type 2 error

---
+++ probablity-lecture-1/type1&type2-error.py
@@ -0,0 +1,66 @@
+# Step 1: Understand Hypothesis
+# There are two hypotheses:
+
+# Null Hypothesis (H₀) → Default assumption
+# Alternative Hypothesis (H₁ or Ha) → Opposite claim
+
+# Example:
+# H₀: “Student is not cheating.”
+# H₁: “Student is cheating.”
+# Now while making decisions, mistakes can happen.
+
+# Type I Error
+# Meaning
+# Rejecting the null hypothesis when it is actually true.
+# 👉 Means: You say something is wrong, but actually it was correct.
+
+# H₀: Patient is healthy
+# H₁: Patient is sick
+# Doctor says patient is sick, but actually patient was healthy.
+
+# ➡️ This is Type I Error.
+# Simple Line
+# Innocent person punished.
+
+# Type II Error
+# Meaning
+# Failing to reject the null hypothesis when it is actually false.
+# 👉 Means: You say everything is fine, but actually there was a problem.
+
+
+# Example
+# H₀: Patient is healthy
+# H₁: Patient is sick
+# Doctor says patient is healthy, but actually patient was sick.
+
+# ➡️ This is Type II Error.
+# Simple Line
+# Guilty person released.
+
+# Easy Comparison Table
+# Basis	Type I Error	Type II Error
+# Symbol	α (Alpha)	β (Beta)
+# Meaning	Reject true H₀	Accept false H₀
+# Decision Wrong?	Yes	Yes
+# Example	Healthy called sick	Sick called healthy
+# Court Example	Innocent punished	Criminal released
+# Memory Trick
+# Error	Trick
+# Type I	“I” = Innocent punished
+# Type II	“II” = Guilty escapes
+# Real-Life Example (Exam)
+
+# Suppose:
+# H₀: Student did not cheat
+# H₁: Student cheated
+# Type I Error
+# Teacher says student cheated, but actually student didn’t.
+
+# Type II Error
+# Teacher says student didn’t cheat, but actually student cheated.
+
+# Important Point
+# In hypothesis testing:
+# Lower α → fewer Type I errors
+# But sometimes Type II errors increase.
+# So both errors are balanced carefully.
