3ff3501d2e3fffda95d01eff2c1e45c884c09ccc
altafshaikh7 <altafshaikh07781@gmail.com>
2026-05-21 12:28:23 +0530

    type 1 and type 2 error

--- probablity-lecture-1/type1&type2-error.py
+++ probablity-lecture-1/type1&type2-error.py
@@ -59,8 +59,3 @@
 # Type II Error
 # Teacher says student didn’t cheat, but actually student cheated.
 
-# Important Point
-# In hypothesis testing:
-# Lower α → fewer Type I errors
-# But sometimes Type II errors increase.
-# So both errors are balanced carefully.
