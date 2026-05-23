e02567afa50c10d955e3c3cc1454c9a97d797e49
altafshaikh7 <altafshaikh07781@gmail.com>
2026-05-23 11:27:08 +0530

    Z test

---
+++ probablity-lecture-1/Z-test.py
@@ -0,0 +1,23 @@
+# A Z-test is a statistical test used to check whether there is a significant difference between:
+
+# a sample mean and population mean, or
+# two sample means/proportions
+
+# It is mainly used when:
+
+# sample size is large (n≥30)
+# population standard deviation (σ) is known
+
+
+# | Type              | Use                                      |
+# | ----------------- | ---------------------------------------- |
+# | One-sample Z-test | Compare sample mean with population mean |
+# | Two-sample Z-test | Compare two sample means                 |
+# | Proportion Z-test | Compare proportions                      |
+
+
+# Key Points
+# Used for large samples
+# Uses normal distribution
+# Faster and simpler than T-test
+# If population SD is unknown and sample is small → use T-test instead
